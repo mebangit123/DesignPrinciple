dbccd7b302b59d4f8ff40cb8c66be47892bf1428
mebangit123 <mebannongrum333@gmail.com>
2021-05-17 16:22:52 +0530

    Created StateCensusAnalyzer Class to load the State census CSV Data

---
+++ DesignPrinciple/src/main/java/com/day29/census/CensusAnalyzer.java
@@ -0,0 +1,20 @@
+package com.day29.census;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class CensusAnalyzer {
+	public void loadIndiaCensusData(String csvFilePath) throws IOException  {
+		try(Reader reader = Files.newBufferedReader(Paths.get(csvFilePath));)  {
+			CsvToBeanBuilder<IndiaCensusCSV> csvToBeanBuilder = new CsvToBeanBuilder<>(reader);
+			csvToBeanBuilder.withType(IndiaCensusCSV.class);
+			csvToBeanBuilder.withIgnoreLeadingWhiteSpace(true);
+			CsvToBean<IndiaCensusCSV> csvToBean = csvToBeanBuilder.build();
+		} 
+	}
+}
